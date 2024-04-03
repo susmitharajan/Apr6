850610888922ccf8ed7cfc6435b9b21d6e299e1a
susmitha sundararajan <susmithasundararajan6@gmail.com>
2024-04-03 11:13:24 +0530

    First commt

--- MyApplication/app/src/main/java/com/susmitharajan/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/susmitharajan/myapplication/MainActivity.java
@@ -20,5 +20,8 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //Testing Github
+        // remote origin git https://github.com/susmitharajan/myapp.git
     }
 }
